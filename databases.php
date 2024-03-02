6a93f7c309a2a276b31d3ff2475d80809023abc7
Kawsar Ahmed Ashif <96182258+kawsarbp@users.noreply.github.com>
2024-03-02 21:07:23 +0600

    Inserting, Updating and Deleting Records

--- databases.php
+++ databases.php
@@ -10,6 +10,14 @@
 
 "SHOW CREATE TABLE `products`"; // table creation process
 
+"TRUNCATE `users`"; // truncate table empty all data
+
+"SHOW WARNINGS"; // show warning
+"SELECT @@warning_count"; // show warning count
+
+"SHOW ERRORS"; // show errors
+"SELECT @@error_count"; // show error count
+
 "SELECT `name`,`quantity` FROM `products`"; // select specific product
 
 "SELECT * FROM `products`"; // select all product
@@ -31,4 +39,12 @@
 "SELECT `id`,`name`,`buying_price` ,`quantity` FROM `products` WHERE `buying_price` BETWEEN 800 AND 850 ORDER BY `quantity` DESC,`id` DESC"; // multi sorting with order by
 
 "SELECT `id`, `name` FROM `products`  LIMIT 3 OFFSET 3"; // limit and offset
+
+"INSERT INTO `users`(`name`, `email`, `password`) VALUES ('ashif hossain','ashifhossain1512@gmail.com','12123123')"; // insert data query
+
+"INSERT INTO `users`(`name`, `email`, `password`) VALUES ('ashif hossain','ashifhossain1512@gmail.com','12123123'), ('ashif hossain','ashifhossain1512@gmail.com','12123123')"; // multiple insert data query
+
+"UPDATE `users` SET `name`='ashif' WHERE `id` = 4"; // update data query
+
+"DELETE FROM `users` WHERE `id` = 4"; // delete data query
 
